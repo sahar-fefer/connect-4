c9927291a7211ae7628c634dea619f13d15aff10
sahar feferovich <sahar.fefer@gmail.com>
2020-11-07 12:37:33 +0200

    validate

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,10 +8,6 @@ class Home extends React.Component {
         this.state = {
             rows: undefined,
             col: undefined,
-            player1Name: 'player1',
-            player1color: '#8B008B',
-            player2Name: 'player2',
-            player2color: '#00CED1',
             numOfPlayers: undefined,
             theBestOf: 1
         };
@@ -33,33 +29,41 @@ class Home extends React.Component {
         this.props.changeToTable('rows', rows);
     };
 
+    setBoardSize = (col, rows) => {
+        this.setState({
+            'col': col,
+            'rows': rows
+        });
+    };
+
     render() {
+        const {numOfPlayers, col} = this.state;
         return (
             <div className="home">
-                <div className="wrapper container">
+                <div className="wrapper">
                     <h1 className="header">4 In A Row</h1>
                     <div className='row square_wrapper'>
-                        <button className="square player_square col" onClick={() => this.setNumOfPlayers(1)}>
+                        <button className={`square player_square ${numOfPlayers === 1 ? 'chosen' : ''} col`} onClick={() => this.setNumOfPlayers(1)}>
                             <FaUserAstronaut onClick={() => this.setNumOfPlayers(1)} />
                         </button>
-                        <button className="square player_square col" onClick={() => this.setNumOfPlayers(2)}>
+                        <button className={`square player_square ${numOfPlayers === 2 ? 'chosen' : ''} col`} onClick={() => this.setNumOfPlayers(2)}>
                             <FaUserNinja onClick={() => this.setNumOfPlayers(2)} />
                             <FaUserSecret onClick={() => this.setNumOfPlayers(2)} />
                         </button>
                     </div>
                     <div className='row square_wrapper'>
-                        <button className="square board_square s col" onClick={() => this.setBoardSize(5, 5)}>
+                        <button className={`square board_square s ${col === 5 ? 'chosen' : ''} col`} onClick={() => this.setBoardSize(5, 5)}>
                             S
                         </button>
-                        <button className="square board_square m col" onClick={() => this.setBoardSize(6, 5)}>
+                        <button className={`square board_square m ${col === 6 ? 'chosen' : ''} col`} onClick={() => this.setBoardSize(6, 5)}>
                             M
                         </button>
-                        <button className="square board_square l col" onClick={() => this.setBoardSize(7, 5)}>
+                        <button className={`square board_square l ${col === 7 ? 'chosen' : ''} col`} onClick={() => this.setBoardSize(7, 5)}>
                             L
                         </button>
                     </div>
                     <div className="row">
-                        <Link className="square play col" to="/tabel"><FaPlay /></Link>
+                        <Link className="square play col" to={`${numOfPlayers & col ? '/tabel' : '/'}`}><FaPlay /></Link>
                     </div>
                 </div>
             </div>
